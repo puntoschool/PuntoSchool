4dcc9e0f42b9b3c971472bfd732456d533895dbd
Hugo Reyes <hugo.reyes.casanova@gmail.com>
2020-01-16 21:28:39 -0600

    Initial commit

---
+++ functions.js
@@ -0,0 +1,9 @@
+$("input:checkbox").click(function() {
+  if ($(this).is(":checked")) {
+      var group = "input:checkbox[name='" + $(this).attr("name") + "']";
+      $(group).prop("checked", false);
+      $(this).prop("checked", true);
+  } else {
+      $(this).prop("checked", false);
+  }
+});
